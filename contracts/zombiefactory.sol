5aafe39a61732810b205520c7a40454809fa1e9c
uijin <justwaves@naver.com>
2020-05-02 16:32:50 +0900

    Lesson 3

--- contracts/zombiefactory.sol
+++ contracts/zombiefactory.sol
@@ -1,15 +1,20 @@
 pragma solidity >=0.5.6 <0.7.0;
 
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract ZombieFactory {
+
+contract ZombieFactory is Ownable {
     event NewZombie(uint256 zombieId, string name, uint256 dna);
 
     uint256 dnaDigits = 16;
     uint256 dnaModulus = 10**dnaDigits;
+    uint256 cooldownTime = 1 days;
 
     struct Zombie {
         string name;
         uint256 dna;
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -18,7 +23,7 @@ contract ZombieFactory {
     mapping(address => uint256) ownerZombieCount;
 
     function _createZombie(string memory _name, uint256 _dna) internal {
-        zombies.push(Zombie(_name, _dna));
+        zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime)));
         uint256 id = zombies.length + 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
